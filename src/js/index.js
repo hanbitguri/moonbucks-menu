8f676adcaeda3bcf155276e9444db3d6c5cf54aa
전한빛 <asjask123@gmail.com>
2023-01-28 18:25:03 +0900

    ✨ Implement : add menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,36 @@
+const $ = (selector) => document.querySelector(selector)
 function App(){
-    const addEspressoMenuField = $('#espresso-menu-name')
-    .addEventListener('keypress',(e)=>{
-        if(e.key==='Enter') console.log(e.target.value)
-    })
-    const addEspressoMenuButton = document.querySelector('#espresso-menu-submit-button')
-    const addEspressoForm = document.querySelector('#espresso-menu-form')
-    .addEventListener('submit',(e)=>{
+    const addEspressoForm = $('#espresso-menu-form').addEventListener('submit',(e)=>{
         e.preventDefault();
     })
+
+    $('#espresso-menu-name').addEventListener('keypress',(e)=>{
+        if(e.key==='Enter'){
+            const espressoMenuName = $('#espresso-menu-name').value;
+            const espressoTemplate = (espressoMenuName) => {
+                return `
+                <li class="menu-list-item d-flex items-center py-2">
+                    <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+                    <button
+                      type="button"
+                      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                    >
+                      수정
+                    </button>
+                    <button
+                      type="button"
+                      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                    >
+                      삭제
+                    </button>
+                </li>`;
+            };
+            $('#espresso-menu-list').innerHTML = espressoTemplate(espressoMenuName);
+        }
+    })
+    
+    
+    
+   
 }
-const $ = (selector) => document.querySelector(selector)
-App()
+App();
